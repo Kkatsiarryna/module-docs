274671c4642431ace5b446ac18c66e1356db246b
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2025-10-14 17:27:12 +0300

    fix: remove not nesessary elements from App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,10 @@
 import { StyledEngineProvider } from '@mui/material/styles'
-import { FilterListItem, FilterUsers, ResponsiveAppBar, SelectRole } from '../shared/ui'
 
 function App() {
   return (
     <StyledEngineProvider injectFirst>
       <div style={{ display: 'flex', flexDirection: 'column', gap: '50px', marginLeft: '10%' }}>
         Hello World!
-        <ResponsiveAppBar></ResponsiveAppBar> <SelectRole></SelectRole>{' '}
-        <FilterListItem nameArray={'sortDates'}></FilterListItem> <FilterUsers></FilterUsers>
       </div>
     </StyledEngineProvider>
   )
