1278e60ecd8b5daae1fe36e76d2174fd49f19fa3
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2025-10-17 22:30:45 +0300

    feat: start creating the modal element

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import { StyledEngineProvider } from '@mui/material/styles'
+import Modal from '../shared/ui/modal/Modal'
 
 function App() {
   return (
     <StyledEngineProvider injectFirst>
       <div style={{ display: 'flex', flexDirection: 'column', gap: '50px', marginLeft: '10%' }}>
         Hello World!
+        <Modal></Modal>
       </div>
     </StyledEngineProvider>
   )
