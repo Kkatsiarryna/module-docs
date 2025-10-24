88d1aa9fa8730cce2a5f5b1cc37b8c8939fea897
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2025-10-24 22:48:23 +0300

    refactor: remove icons tsx to model folder

--- src/shared/ui/dropdowns/dropdownRole/dropdownRole.tsx
+++ src/shared/ui/dropdowns/dropdownRole/dropdownRole.tsx
@@ -7,7 +7,7 @@ import { styled } from '@mui/material/styles'
 import styles from './DropdownRole.module.scss'
 import { rolesUsers } from '@shared/model/users'
 import { Box } from '@mui/material'
-import { Icon } from '../../icons/icons-helper'
+import { Icon } from '../../../model/icons'
 import { ICONS, SIZES_ICON } from '../../icons/icons'
 
 const MenuProps = {
