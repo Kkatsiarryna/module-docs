a2e95ec86d607fe440f2156f78dad25ecb1a8c00
kkatsiarryna <katepicastor@gmail.com>
2025-10-28 05:29:50 +0300

    de/refactor: change name folder validation

--- src/features/auth/model/schemas/confirm-password.schema.ts
+++ src/features/auth/model/schemas/confirm-password.schema.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod'
-import { passwordSchema } from '@shared/model/validations'
+import { passwordSchema } from '@shared/model/validation'
 
 export const confirmPasswordSchema = z.object({
   password: passwordSchema,
--- src/features/auth/model/schemas/login-form.schema.ts
+++ src/features/auth/model/schemas/login-form.schema.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod'
-import { emailSchema, passwordSchema } from '@shared/model/validations'
+import { emailSchema, passwordSchema } from '@shared/model/validation'
 
 export const loginSchema = z.object({
   email: emailSchema,
