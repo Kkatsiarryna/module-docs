d8c4443892339b9c86f757afac3b42d5a405297c
kkatsiarryna <katepicastor@gmail.com>
2025-10-30 18:56:32 +0300

    de/build image

--- src/pages/documents/ui/documents-page.tsx
+++ src/pages/documents/ui/documents-page.tsx
@@ -1,3 +1,29 @@
 export const DocumentsPage = () => {
-  return <div>Documents Page</div>
+  //return <div>Documents Page</div>
+  return (
+    <>
+      <div style={{ margin: '20px' }}>Documents Page</div>
+      {/*<div style={{ width: '200px', margin: '20px' }}>*/}
+      {/*  <InputSearch />*/}
+      {/*</div>*/}
+      {/*<div style={{ width: '200px', margin: '20px' }}>*/}
+      {/*  <Input placeholder={'Placeholder'} />*/}
+      {/*</div>*/}
+      {/*<div style={{ width: '200px', margin: '20px' }}>*/}
+      {/*  <Input placeholder={'Placeholder'} type="password" />*/}
+      {/*</div>*/}
+      {/*<div style={{ width: '200px', margin: '20px' }}>*/}
+      {/*  <Input placeholder={'Placeholder'} type="password" label="Password" />*/}
+      {/*</div>*/}
+      {/*<div style={{ width: '200px', margin: '20px' }}>*/}
+      {/*  <Input placeholder={'Placeholder'} type="password" label="Password" size={'XS'} />*/}
+      {/*</div>*/}
+      {/*<div style={{ width: '200px', margin: '20px' }}>*/}
+      {/*  <Input size={'XS'} />*/}
+      {/*</div>*/}
+      {/*<div style={{ width: '200px', margin: '20px' }}>*/}
+      {/*  <Input placeholder={'Placeholder'} label="Password" helperText={'Password'} />*/}
+      {/*</div>*/}
+    </>
+  )
 }
