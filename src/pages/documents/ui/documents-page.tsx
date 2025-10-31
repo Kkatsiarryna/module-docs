c214ed4fa243ba233ccf9ae439da30f2cf0d3717
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2025-10-31 04:23:41 +0300

    feat: add sidebar

--- src/pages/documents/ui/documents-page.tsx
+++ src/pages/documents/ui/documents-page.tsx
@@ -1,7 +1,9 @@
+// import Tab from "@shared/ui/tab/Tab"
+
 export const DocumentsPage = () => {
   return (
     <>
-      <div style={{ margin: '20px' }}>Documents Page</div>
+      <div>Documents Page</div>
     </>
   )
 }
--- src/widgets/app-layout/AppLayout.tsx
+++ src/widgets/app-layout/AppLayout.tsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom'
 import { Header } from '@shared/ui'
 import styles from './AppLayout.module.scss'
 import type { ReactNode } from 'react'
+import Tab from '@shared/ui/tab/Tab'
 
 export const AppLayout = ({ children }: { children: ReactNode }) => {
   const { pathname } = useLocation()
@@ -15,7 +16,7 @@ export const AppLayout = ({ children }: { children: ReactNode }) => {
         {!isHideHeader && (
           <Box className={styles.sidebarWrapper}>
             {/*<SideBar />*/}
-            SideBar
+            <Tab></Tab>
           </Box>
         )}
         <Box className={styles.content}>{children}</Box>
