c7c2c7047a47ce48a3d992126cd38f2f9ed1617b
kkatsiarryna <katepicastor@gmail.com>
2025-11-01 23:30:52 +0300

    de/refactor: return previous

--- src/pages/documents/ui/documents-page.tsx
+++ src/pages/documents/ui/documents-page.tsx
@@ -1,17 +1,7 @@
-import { Input, Select } from '@shared/ui'
-import { CreateForm } from '@shared/ui/create-form/CreateForm.tsx'
-
 export const DocumentsPage = () => {
-  const role = ['Admin', 'HR', 'Manager']
-
   return (
     <>
-      <CreateForm title={'Добавление пользователя'}>
-        <Input label={'Имя'} required />
-        <Input label={'Фамилия'} required />
-        <Select placeholder={'Роль'} selectItems={role} />
-        <Input label={'Email'} required />
-      </CreateForm>
+      <div>Documents Page</div>
     </>
   )
 }
