57ea77d6bc831f0f5e4142bc36c720106bd85e4c
kkatsiarryna <katepicastor@gmail.com>
2025-11-01 23:09:52 +0300

    de/feat: add hook useAdminPage

--- src/pages/admin/ui/AdminPage.tsx
+++ src/pages/admin/ui/AdminPage.tsx
@@ -1,8 +1,12 @@
 import Box from '@mui/material/Box'
 import { Button, InputSearch, Typography } from '@shared/ui'
 import styles from './AdminPage.module.scss'
+import { useAdminPage } from '@pages/admin/model'
+import { AddUserForm } from '@features/user-management/ui'
 
 export const AdminPage = () => {
+  const { isAddUserModalOpen, openAddUserModal, closeAddUserModal } = useAdminPage()
+
   return (
     <div className={styles.adminPage}>
       <Typography variant={'heading1'} className={styles.title}>
@@ -10,10 +14,12 @@ export const AdminPage = () => {
       </Typography>
       <Box className={styles.controls}>
         <InputSearch className={styles.search} />
-        <Button variant={'primary'} className={styles.addButton}>
+        <Button variant={'primary'} className={styles.addButton} onClick={openAddUserModal}>
           + Добавить пользователя
         </Button>
       </Box>
+
+      {isAddUserModalOpen && <AddUserForm onClose={closeAddUserModal} open={isAddUserModalOpen} />}
     </div>
   )
 }
