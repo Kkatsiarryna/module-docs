d0dcf27749068b55877d3e92f1e26b9817cf5e74
kkatsiarryna <katepicastor@gmail.com>
2025-11-05 01:10:10 +0300

    de/ci: nginx conf v4

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -1,6 +1,5 @@
-// const IS_DEV = import.meta.env.MODE === 'development'
+const IS_DEV = import.meta.env.MODE === 'development'
 
 export const config = {
-  // BASE_URL: IS_DEV ? '/api' : window.__ENV__.BASE_URL,
-  BASE_URL: 'http://it-camp-team-4-be.ihub-2pmgames.by',
+  BASE_URL: IS_DEV ? '/api' : window.__ENV__.BASE_URL,
 }
