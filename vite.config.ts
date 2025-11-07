278deecd501f0f64b59f2063eecb6eff82026713
kkatsiarryna <katepicastor@gmail.com>
2025-11-07 20:04:24 +0300

    de/fix: vite config target url backoffice

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8080',
-        //target: 'http://backoffice-service-team-4.ihub-2pmgames.by',
+        //target: 'http://127.0.0.1:8080',
+        target: 'https://backoffice-service-team-4.ihub-2pmgames.by',
         changeOrigin: true,
         //rewrite: path => path.replace(/^\/api/, ''),
         secure: false,
