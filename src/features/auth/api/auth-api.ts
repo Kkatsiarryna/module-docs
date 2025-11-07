40a4b6ea62bdad627517b36e3797b6dd400f3bbb
kkatsiarryna <katepicastor@gmail.com>
2025-11-07 03:41:40 +0300

    de/fix: user type

--- src/features/auth/api/auth-api.ts
+++ src/features/auth/api/auth-api.ts
@@ -1,11 +1,23 @@
 import { baseApi } from '@shared/api'
-import type { CreatePasswordRequest, User } from '@features/auth/model'
+import type { CreatePasswordRequest, MeResponse, User } from '@features/auth/model'
+import { TokenUserRoles } from '@shared/model/user'
 
 export const authApi = baseApi.injectEndpoints({
   endpoints: build => ({
     me: build.query<User, void>({
       query: () => 'users/me',
       providesTags: ['User'],
+      transformResponse: (response: MeResponse): User => {
+        const d = response.data
+        return {
+          id: d.id,
+          email: d.email,
+          firstname: d.first_name,
+          lastname: d.last_name,
+          avatar: d.file_link || undefined,
+          role: d.role.name as keyof typeof TokenUserRoles,
+        }
+      },
     }),
     login: build.mutation({
       query: credentials => ({
