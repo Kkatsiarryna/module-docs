ca0d9624bfcc9e8b40bcb42563695da0224d1235
kkatsiarryna <katepicastor@gmail.com>
2025-11-07 15:44:16 +0300

    de/fix: add token to confirm password

--- src/features/auth/model/hooks/use-confirm-password-form.ts
+++ src/features/auth/model/hooks/use-confirm-password-form.ts
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { CONFIRM_PASSWORD_SCHEMA } from '@features/auth/model'
 import { useConfirmPasswordMutation } from '@features/auth/api'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import { routes } from '@shared/config'
 import { useState } from 'react'
 import type { ConfirmPasswordType } from '@features/auth/model/schemas/confirm-password.schema.ts'
@@ -10,7 +10,10 @@ import type { ConfirmPasswordType } from '@features/auth/model/schemas/confirm-p
 export const useConfirmPasswordForm = () => {
   const [confirmPasswordMutation, { isLoading }] = useConfirmPasswordMutation()
   const navigate = useNavigate()
+  const [searchParams] = useSearchParams()
   const [isFocused, setIsFocused] = useState(false)
+
+  const token = searchParams.get('token')
 
   const form = useForm({
     mode: 'onChange',
@@ -30,8 +33,13 @@ export const useConfirmPasswordForm = () => {
   const showHint = isFocused || password.length > 0
 
   const onSubmit = async ({ password }: ConfirmPasswordType) => {
+    if (!token) {
+      console.error('Token not found in URL')
+      return
+    }
+
     try {
-      await confirmPasswordMutation({ new_password: password }).unwrap()
+      await confirmPasswordMutation({ new_password: password, token: token }).unwrap()
       navigate(routes.documents, { replace: true })
     } catch (error) {
       console.log('Ошибка подтверждения пароля:', error)
