6b831c43d3ed4520ece1d5fcfd8dcef02c6afd6f
kkatsiarryna <katepicastor@gmail.com>
2025-11-24 17:04:50 +0300

    de/feat: add table users

--- src/pages/documents/ui/DocumentsPage.tsx
+++ src/pages/documents/ui/DocumentsPage.tsx
@@ -1,3 +1,10 @@
+import { UsersTable } from '@widgets/users-table/ui'
+import styles from './DocumentsPage.module.scss'
+
 export const DocumentsPage = () => {
-  return <> </>
+  return (
+    <div className={styles.page}>
+      <UsersTable />
+    </div>
+  )
 }
