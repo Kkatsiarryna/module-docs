d5d9fdcc383ec49d8bbe96ac84a9623c275693dd
kkatsiarryna <katepicastor@gmail.com>
2025-11-25 17:12:38 +0300

    de/refactor: move the business logic into a hook

--- src/widgets/users-table/ui/UsersTable.tsx
+++ src/widgets/users-table/ui/UsersTable.tsx
@@ -1,57 +1,26 @@
 import styles from './UsersTable.module.scss'
 import { DataGrid } from '@mui/x-data-grid/DataGrid'
 import Box from '@mui/material/Box'
+import { Pagination } from '@shared/ui'
+import { useUsersTable } from '@widgets/users-table/model'
 
 export const UsersTable = () => {
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 80 },
-    {
-      field: 'firstname',
-      headerName: 'Имя',
-      flex: 1,
-    },
-    {
-      field: 'lastname',
-      headerName: 'Фамилия',
-      flex: 1,
-    },
-    {
-      field: 'role',
-      headerName: 'Роль',
-      editable: true,
-      flex: 1,
-    },
-    {
-      field: 'email',
-      headerName: 'Почта',
-      flex: 1.5,
-    },
-  ]
-  //const rows = []
-  const rows = Array.from({ length: 100 }, (_, i) => ({
-    id: i + 1,
-    firstname: `Имя${i + 1}`,
-    lastname: `Фамилия${i + 1}`,
-    role: i % 2 === 0 ? 'Admin' : 'User',
-    email: `user${i + 1}@example.com`,
-  }))
+  const { paginatedRows, columns, page, setPage, totalPages } = useUsersTable()
 
   return (
     <Box className={styles.dataGrid}>
       <DataGrid
-        rows={rows}
+        rows={paginatedRows}
         columns={columns}
-        disableColumnMenu={true}
-        // initialState={{
-        //   pagination: {
-        //     paginationModel: {
-        //       pageSize: 10,
-        //     },
-        //   },
-        // }}
-        // pageSizeOptions={[10]}
-        // disableRowSelectionOnClick
+        disableColumnMenu
+        hideFooter
+        disableRowSelectionOnClick
+        autoHeight
       />
+
+      <Box className={styles.paginationWrapper}>
+        <Pagination count={totalPages} page={page} onChange={(_, value) => setPage(value)} />
+      </Box>
     </Box>
   )
 }
